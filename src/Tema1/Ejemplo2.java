01cc683b1d11e3ff879e51d9887a5e20e4115a73
davidmartin99 <davidmartinpulgar9@gmail.com>
2024-09-26 12:57:41 +0200

    programación

--- src/Tema1/Ejemplo2.java
+++ src/Tema1/Ejemplo2.java
@@ -5,15 +5,15 @@ import java.io.InputStream;
 
 public class Ejemplo2 {
     public static void main(String[] args) throws IOException {
+
+        // Creamos un proceso para ejecutar el comando CMD
+        Process p = new ProcessBuilder("CMD", "/C", "DIR").start();
+
         try {
-            // Creamos un proceso para ejecutar el comando CMD
-            Process p = new ProcessBuilder("CMD", "/C", "DIR").start();
-            
             // Obtenemos el flujo de entrada del proceso
             InputStream is = p.getInputStream();
             
             // Mostramos en pantalla el resultado del proceso, caracter por caracter
-            // COMPROBACION DE ERROR - 0 BIEN - 1 MAL
             int c;
             while ((c = is.read()) != -1) {
                 System.out.print((char) c);
@@ -22,6 +22,17 @@ public class Ejemplo2 {
             // Cerramos el flujo de entrada
             is.close();
         } catch (Exception e) {
+            e.printStackTrace();
+        }//Fin try-catch
+
+
+        // COMPROBACION DE ERROR - 0   BIEN - 1 MAL  usamos waitFor()
+        int exitVal;
+        try{
+            //Se recoge la salida de Sysyem.exit()
+            exitVal = p.waitFor();
+            System.out.println("Valor de Salida: " + exitVal);
+        }catch(InterruptedException e) {
             e.printStackTrace();
         }//Fin try-catch
         
