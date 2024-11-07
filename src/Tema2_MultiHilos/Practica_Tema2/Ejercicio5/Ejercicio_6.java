a10613ce44aa08b2d52468c75f6e583960dfa16e
davidmartin99 <davidmartinpulgar9@gmail.com>
2024-11-07 08:49:40 +0100

    Practica Tema2 07/11.

--- src/Tema2_MultiHilos/Practica_Tema2/Ejercicio5/Ejercicio_6.java
+++ src/Tema2_MultiHilos/Practica_Tema2/Ejercicio5/Ejercicio_6.java
@@ -13,8 +13,8 @@ public class Ejercicio_6 {
     public static void main(String[] args) {
         // Crea tres hilos con diferentes prioridades
         HiloPrioridades hilo1 = new HiloPrioridades("Hilo 1", 1);
-        HiloPrioridades hilo2 = new HiloPrioridades("Hilo 3", 3);
-        HiloPrioridades hilo3 = new HiloPrioridades("Hilo 5", 5);
+        HiloPrioridades hilo2 = new HiloPrioridades("Hilo 2", 3);
+        HiloPrioridades hilo3 = new HiloPrioridades("Hilo 3", 5);
 
         // Inicia los hilos
         hilo1.start();
